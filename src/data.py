b7799a4b593bb51274e5ab6abceb80ea90af600b
alexgiving <alexgiving@mail.ru>
2022-11-12 00:52:13 +0300

    init

---
+++ src/data.py
@@ -0,0 +1,28 @@
+from torchvision import transforms, datasets
+import torch
+
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+def data_loader(data_dir,
+                batch_size,
+                shuffle=True,
+                num_workers=4,
+                ):
+
+    # define transforms
+    transform = transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010))
+    ])
+
+    trainset = datasets.CIFAR10(root=data_dir, train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+    testset = datasets.CIFAR10(root=data_dir, train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return {'train': trainloader, 'test': testloader}
