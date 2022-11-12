a8f885869a81e1f0092fb02a21548533f51875a7
alexgiving <alexgiving@mail.ru>
2022-11-12 12:53:06 +0300

    new path

--- scripts/run_custom_train.py
+++ scripts/run_custom_train.py
@@ -2,79 +2,78 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
-import torch.backends.cudnn as cudnn
-import numpy as np
 import torchvision
-from torchvision import datasets, models, transforms
-import time
-import os
-import copy
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-print(device, flush=True)
+import click
 
 
-import sys
-import os
-sys.path.append(os.path.dirname('..'))
+@click.command()
+@click.option('--ckpt', default='ckpt/', help='Directory for storing data')
+def main(ckpt):
 
-from src.data import data_loader, classes
-from src.train import train_model
-from src.model import ResNet18
+    import sys
+    import os
+    sys.path.append(os.path.dirname('..'))
 
-
-num_classes = 10
-num_epochs = 25
-batch_size = 5
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    from src.data import data_loader
+    from src.train import train_model
+    from src.model import ResNet18
 
 
-# Finetuned model
-print("Finetuned", flush=True)
-model_conv = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.DEFAULT)
+    num_classes = 10
+    num_epochs = 25
+    batch_size = 5
 
-# Freeze training for all layers exept the last one for finetuning
-for param in model_conv.parameters(): param.requires_grad = False
-num_ftrs = model_conv.fc.in_features
-model_conv.fc = nn.Linear(num_ftrs, num_classes)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device, flush=True)
 
 
-model_conv = model_conv.to(device)
+    # Finetuned model
+    print("Finetuned", flush=True)
+    model_conv = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.DEFAULT)
+
+    # Freeze training for all layers exept the last one for finetuning
+    for param in model_conv.parameters(): param.requires_grad = False
+    num_ftrs = model_conv.fc.in_features
+    model_conv.fc = nn.Linear(num_ftrs, num_classes)
 
 
-criterion = nn.CrossEntropyLoss()
-optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9) # Set small learning rate for finetuning
-exp_lr_scheduler = None
-
-datasets = data_loader(data_dir='data/', batch_size=batch_size)
-dataset_sizes = {phase: len(datasets[phase]) for phase in ['train', 'test']}
+    model_conv = model_conv.to(device)
 
 
-model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, device, datasets, dataset_sizes, num_epochs=num_epochs)
-torch.save(model_conv.state_dict(), 'baseline_resnet_best.pth')
+    criterion = nn.CrossEntropyLoss()
+    optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9) # Set small learning rate for finetuning
+    exp_lr_scheduler = None
 
+    datasets = data_loader(data_dir='data/', batch_size=batch_size)
+    dataset_sizes = {phase: len(datasets[phase]) for phase in ['train', 'test']}
+
+
+    model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, device, datasets, dataset_sizes, num_epochs=num_epochs)
+    torch.save(model_conv.state_dict(), f'{ckpt}baseline_resnet_best.pth')
 
 
 
-
-# Custom model
-print("Custom", flush=True)
-model = ResNet18(num_classes).to(device)
-
-
-criterion = nn.CrossEntropyLoss()
-optimizer_conv = optim.SGD(model.fc.parameters(), lr=0.1, momentum=0.9)
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=5, gamma=0.1)
+    # Custom model
+    print("Custom", flush=True)
+    model = ResNet18(num_classes).to(device)
 
 
-datasets = data_loader(data_dir='data/', batch_size=batch_size)
-dataset_sizes = {phase: len(datasets[phase]) for phase in ['train', 'test']}
+    criterion = nn.CrossEntropyLoss()
+    optimizer_conv = optim.SGD(model.fc.parameters(), lr=0.1, momentum=0.9)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=5, gamma=0.1)
+
+
+    datasets = data_loader(data_dir='data/', batch_size=batch_size)
+    dataset_sizes = {phase: len(datasets[phase]) for phase in ['train', 'test']}
 
 
 
-best_custom_resnet = train_model(model, criterion, optimizer_conv, exp_lr_scheduler, device, datasets, dataset_sizes, num_epochs=num_epochs)
-torch.save(best_custom_resnet.state_dict(), 'custom_resnet_best.pth')
+    best_custom_resnet = train_model(model, criterion, optimizer_conv, exp_lr_scheduler, device, datasets, dataset_sizes, num_epochs=num_epochs)
+    torch.save(best_custom_resnet.state_dict(), f'{ckpt}custom_resnet_best.pth')
 
 
-print("Done", flush=True)
+    print("Done", flush=True)
+
+
+if __name__ == '__main__':
+    main()
